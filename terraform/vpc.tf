9f27c8afe52634c26f4b8df7d1f70a7b6024223d
Joe Richardson <joeyrichardson96@gmail.com>
2023-04-29 21:44:09 +0100

    tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -17,3 +17,13 @@ resource "aws_subnet" "main" {
     Name = "main-subnet"
   }
 }
+
+resource "aws_subnet" "secondary" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    Name = "secondary-subnet"
+  }
+}
