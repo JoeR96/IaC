f8696468c02c7eaa74accd86273d3a03e4aa9882
Joe Richardson <joeyrichardson96@gmail.com>
2023-04-29 21:45:52 +0100

    tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -27,3 +27,34 @@ resource "aws_subnet" "secondary" {
     Name = "secondary-subnet"
   }
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "secondary" {
+  subnet_id      = aws_subnet.secondary.id
+  route_table_id = aws_route_table.main.id
+}
